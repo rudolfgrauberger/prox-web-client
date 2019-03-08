ccc5fc98d15c2ecdd32ac693c7f7655f26e8516c
JannIntveen <JannIntveen@gmail.com>
2019-03-08 11:12:53 +0100

    Added Dummy Module selection inside create project dialog

--- src/app/components/project-dialog/project-dialog.component.ts
+++ src/app/components/project-dialog/project-dialog.component.ts
@@ -3,6 +3,21 @@ import {MatDialogRef, MatSnackBar} from '@angular/material';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ProjectService} from '../../core/services/project.service';
 import {Project} from '../../shared/hal-resources/project.resource';
+import {Module} from '../../shared/hal-resources/module.resource';
+
+export class DummyModule {
+  name: string
+}
+
+export const MODULES: DummyModule[] = [
+  { name: 'Module 1'},
+  { name: 'Module 2'},
+  { name: 'Module 3'},
+  { name: 'Module 4'},
+  { name: 'Module 5'},
+  { name: 'Module 6'},
+  { name: 'Module 7'},
+];
 
 @Component({
   selector: 'app-project-dialog',
@@ -11,6 +26,8 @@ import {Project} from '../../shared/hal-resources/project.resource';
 })
 export class ProjectDialogComponent implements OnInit {
   projectFormControl: FormGroup;
+  modules = MODULES;
+  selectedModules: DummyModule[] = [];
 
   constructor(public projectDialogRef: MatDialogRef<ProjectDialogComponent>,
               private projectService: ProjectService,
@@ -28,6 +45,17 @@ export class ProjectDialogComponent implements OnInit {
 
   onClose() {
     this.projectDialogRef.close();
+  }
+
+  onSelectModule(module: DummyModule) {
+    if (this.selectedModules.includes(module)) {
+      const index = this.selectedModules.indexOf(module, 0);
+      if (index > -1) {
+        this.selectedModules.splice(index, 1);
+      }
+    } else {
+      this.selectedModules.push(module);
+    }
   }
 
   onSubmit(project: Project) {
