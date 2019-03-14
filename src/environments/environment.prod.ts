bb98dd9668af76b67431b39f477f406c20bd8ddb
Florian Baldus <florian.baldus@t-online.de>
2019-03-14 13:56:12 +0100

    Keycloak added (Login/Logout, route security)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
+import {KeycloakConfig} from 'keycloak-angular';
+
+const keycloakConfig: KeycloakConfig = {
+  url: 'http://localhost:8080/auth',
+  realm: 'PTB',
+  clientId: 'ptb-web-client'
+};
+
 export const environment = {
   production: true,
+  keycloak: keycloakConfig,
   url: 'https://gptest.archi-lab.io/'
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,16 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {KeycloakConfig} from 'keycloak-angular';
+
+const keycloakConfig: KeycloakConfig = {
+  url: 'http://localhost:8080/auth',
+  realm: 'PTB',
+  clientId: 'ptb-web-client'
+};
+
 export const environment = {
   production: false,
-  url: 'http://localhost:9002'
+  keycloak: keycloakConfig,
+  url: 'https://gptest.archi-lab.io/'
 };
