8dcaad269d8f72f78e75c9d66b6af4ce811be6b2
Florian Baldus <florian.baldus@t-online.de>
2019-03-29 12:18:23 +0100

    keycloak init code cleanup

--- src/app/keycloak/keycloak-init.ts
+++ src/app/keycloak/keycloak-init.ts
@@ -4,25 +4,13 @@ import {environment} from '../../environments/environment';
 
 export function keycloakInitializer(keycloak: KeycloakService): () =>
   Promise<any> {
-  if (environment.production) {
-    return () => keycloak.init({
-      config: environment.keycloak,
-      initOptions: {
-        onLoad: 'check-sso',
-        checkLoginIframe: true,
-      },
-      // disable default interceptor, we use custom interceptor(bearer.interceptor.ts)
-      enableBearerInterceptor: false,
-    });
-  } else {
-    return () => keycloak.init({
-      config: environment.keycloak,
-      initOptions: {
-        onLoad: 'check-sso',
-        checkLoginIframe: true,
-      },
-      // disable default interceptor, we use custom interceptor(bearer.interceptor.ts)
-      enableBearerInterceptor: false
-    });
-  }
+  return () => keycloak.init({
+    config: environment.keycloak,
+    initOptions: {
+      onLoad: 'check-sso',
+      checkLoginIframe: true,
+    },
+    // disable default interceptor, we use custom interceptor(bearer.interceptor.ts)
+    enableBearerInterceptor: false,
+  });
 }
