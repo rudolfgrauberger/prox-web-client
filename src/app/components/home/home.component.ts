0c3a8491abaac894d7fcb4cb133fb40e0b527619
Florian Baldus <florian.baldus@t-online.de>
2019-04-04 14:25:22 +0200

    redirect dozent from landing page to login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {KeyCloakUser} from '../../keycloak/KeyCloakUser';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(private user: KeyCloakUser, private router: Router) {
   }
 
   ngOnInit() {
   }
 
+
+  editProject() {
+    if (this.user.isLoggedIn()) {
+      this.router.navigate(['/', 'projects']);
+    } else {
+       this.user.loginRedirect(window.location.href + 'projects');
+    }
+  }
 }
