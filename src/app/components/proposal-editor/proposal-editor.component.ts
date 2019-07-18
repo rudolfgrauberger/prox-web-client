4a04ce859154eae0eb1a2a5301379012bd6f892d
cpolocze <40211062+cpolocze@users.noreply.github.com>
2019-07-18 16:29:53 +0200

    Add view mode toggle to proposal-editor

--- src/app/components/proposal-editor/proposal-editor.component.ts
+++ src/app/components/proposal-editor/proposal-editor.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export type ViewMode = 'editor' | 'preview';
+
 @Component({
   selector: 'app-proposal-editor',
   templateUrl: './proposal-editor.component.html',
   styleUrls: ['./proposal-editor.component.scss']
 })
 export class ProposalEditorComponent implements OnInit {
+  viewMode: ViewMode = 'editor';
   constructor() {}
 
   ngOnInit() {}
+
+  get showEditor() {
+    return this.viewMode === 'editor';
+  }
+
+  get showPreview() {
+    return this.viewMode === 'preview';
+  }
+
+  toggleViewMode(type: ViewMode) {
+    this.viewMode = type;
+  }
 }
