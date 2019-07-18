ddf76eb778f143b2df3ebb1e4020da308d6a7a2c
cpolocze <40211062+cpolocze@users.noreply.github.com>
2019-07-18 16:38:04 +0200

    Add functions to access proposal-service and project-service

--- src/app/components/proposal-editor/proposal-editor.component.ts
+++ src/app/components/proposal-editor/proposal-editor.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UUID } from 'angular2-uuid';
 import { ActivatedRoute } from '@angular/router';
+import { Proposal } from '../../shared/hal-resources/proposal-resource';
+import { Project } from '../../shared/hal-resources/project.resource';
+import { ProjectService } from '../../core/services/project.service';
+import { ProposalService } from '../../core/services/proposal.service';
+import { KeyCloakUser } from '../../keycloak/KeyCloakUser';
+import { FormControl } from '@angular/forms';
 
 export type ViewMode = 'editor' | 'preview';
 
@@ -12,12 +18,44 @@ export type ViewMode = 'editor' | 'preview';
 export class ProposalEditorComponent implements OnInit {
   viewMode: ViewMode = 'editor';
   proposalID: UUID;
+  proposal: Proposal;
+  project: Project;
 
-  constructor(private route: ActivatedRoute) {
+  proposalFormControl = new FormControl('');
+
+  constructor(
+    private projectService: ProjectService,
+    private proposalService: ProposalService,
+    private route: ActivatedRoute,
+    private user: KeyCloakUser
+  ) {
     this.route.params.subscribe(params => (this.proposalID = params.id));
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getProposal();
+    this.proposalFormControl.setValue(this.proposal.content);
+  }
+
+  getProposal() {
+    this.proposalService.get(this.proposalID).subscribe(
+      proposal => {
+        this.proposal = proposal;
+        this.proposalFormControl.setValue(proposal.content);
+      },
+      error1 => {},
+      () => this.getProject()
+    );
+  }
+
+  private getProject() {
+    this.projectService.get(this.proposal.projectId).subscribe(project => (this.project = project));
+  }
+
+  patchProposalContent() {
+    this.proposal.content = this.proposalFormControl.value;
+    this.proposalService.patch(this.proposal).subscribe();
+  }
 
   get showEditor() {
     return this.viewMode === 'editor';
